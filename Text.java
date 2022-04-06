f9feedfe39433c3fddb37de10bfe590ec803b883
shubham kesri <shubhamkeshari90@gmail.com>
2022-04-06 23:50:13 +0530

    synced with the changes made by t2

--- Text.java
+++ Text.java
@@ -6,20 +6,16 @@ public class Text {
 	}
 	public void setToken(String token){
 		 this.token = token;
-		 System.out.println("This is something");
-		 System.out.println("I want to test");
-		 System.out.println("to know about merg patterns.");
-		 System.out.println("hence, doing this");
 	}
 
     public void printToken(){
 		System.out.println(this.token);
-		
 	}
 
     
 	public String getToken(){
 		return this.token;
+		
 	}
 	
 }
